7bc8f594a7197f5e9283c69cc3dc47e4d44bca3e
Daniel Coleman <dccol@student.unimelb.edu.au>
2020-05-23 18:05:03 +1000

    loading print no longer overflows

--- src/print_output.c
+++ src/print_output.c
@@ -24,15 +24,16 @@ void print_load(int *pages, int num_pages, const int *space_available, process_t
     int num_process_pages = process->mem_req / PAGE_SIZE;
 
     int *mem_addresses = (int*)malloc(sizeof(*mem_addresses) * num_process_pages);
+    int currently_in_mem = count_process_mem(pages, num_pages, process);
     find_process_mem(pages, num_pages, process, mem_addresses);
 
     // Print
     printf("%d, RUNNING, id=%d, remaining-time=%d, load-time=%d, mem-usage=%2.0lf%%, mem-addresses=[",
            *simulation_time_elapsed, process->pid, process->time_remaining, *loading_cost, mem_usage);
-    for(int i = 0; i < num_process_pages -1 ; i++){
+    for(int i = 0; i < currently_in_mem -1 ; i++){
         printf("%d,", mem_addresses[i]);
     }
-    printf("%d]\n", mem_addresses[num_process_pages-1]);
+    printf("%d]\n", mem_addresses[currently_in_mem-1]);
     free(mem_addresses);
 }
 
--- src/print_output.h
+++ src/print_output.h
@@ -7,7 +7,9 @@
 
 #include "process.h"
 #include "memory_p.h"
+#include "memory_v.h"
 #include "round.h"
+
 
 void print_evicted(process_t *process, int simulation_time_elapsed, int *mem_addresses, int mem_addresses_len);
 
